bb5e9d00197abd08a594b46aaf0dbf49945cc2b6
a107285 <a107285@alunos.uminho.pt>
2024-08-16 17:40:31 +0100

    Contexto receitasvegetarianas e implementacao na side bar do switch para selecionar se o user apenas quer receitas vegetarianas ou nao

---
+++ Contexts/VeganContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState, useContext } from 'react';
+
+// Crie o contexto
+const VeganContext = createContext();
+
+// Crie o provider para o contexto
+export const VeganProvider = ({ children }) => {  // 'children' são os componentes filhos que terão acesso ao contexto
+  const [isVeganChecked, setIsVeganChecked] = useState(false);
+
+  return (
+    <VeganContext.Provider value={{ isVeganChecked, setIsVeganChecked }}>
+      {children} 
+    </VeganContext.Provider>
+  );
+};
+
+// Hook para usar o contexto
+export const useVegan = () => useContext(VeganContext);
